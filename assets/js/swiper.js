b4f621d14a38c4f0d6d3d7962959f725181d4c04
wangkodok <wangkodok@gmail.com>
2025-03-04 22:23:48 +0900

    add：메인 페이지 콘텐츠 영역

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,3 +1,32 @@
 window.addEventListener("load", function () {
+  const sliderLink = document.querySelector(".slider-link");
+  const toggleButton = document.querySelector(".slider-play-button");
+  let isPlaying = true;
 
+  sliderLink.addEventListener("click", (event) => {
+    event.preventDefault();
+  })
+
+  const swiper = new Swiper(".swiper-introBanner", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    loop: true,
+    autoplay: true,
+  });
+
+  toggleButton.addEventListener("click", () => {
+    if (isPlaying) {
+      swiper.autoplay.stop(); // 재생
+      toggleButton.classList.add("passive");
+      toggleButton.classList.remove("active");
+    } else {
+      swiper.autoplay.start(); // 정지
+      toggleButton.classList.add("active");
+      toggleButton.classList.remove("passive");
+    }
+
+    isPlaying = !isPlaying;
+  });
 });
