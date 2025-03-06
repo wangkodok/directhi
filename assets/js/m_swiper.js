12061befeb6e719469d7cf5fc01a79af5a57087f
wangkodok <wangkodok@gmail.com>
2025-03-06 22:36:47 +0900

    add：모바일 버전 메인 페이지 콘텐츠 영역

--- assets/js/m_swiper.js
+++ assets/js/m_swiper.js
@@ -1,3 +1,9 @@
 window.addEventListener("load", function () {
-
+  new Swiper(".swiper-mobileBanner", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    loop: true,
+    autoplay: true,
+  });
 });
