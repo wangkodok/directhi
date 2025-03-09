d3d4eeec168d8f7dc7850f5fecd836e01fc4f2e6
wangkodok <wangkodok@gmail.com>
2025-03-09 22:00:14 +0900

    add：모바일 버전 햄버거 메뉴 영역

--- assets/js/m_gsap.js
+++ assets/js/m_gsap.js
@@ -1,3 +1,61 @@
-window.addEventListener("load", function () {
+window.addEventListener("load", () => {
+  // ------------------------------------------------------
+  // 사용 목적：햄버거 메뉴 클릭 시 네비게이션 클릭 및 메뉴 이동
+  // 사용 언어：javascript + gsap.js
+  // ------------------------------------------------------
+  const hamburgerMenu = document.querySelector(".header .menu-button");
+  const hamburgerArea = document.querySelector(".hamburger-area");
+  const sideNav = document.querySelector(".side-nav");
 
+  hamburgerMenu.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let links = gsap.utils.toArray("#side-nav ul li a");
+    const newArray = links.filter((item, index) => {
+      return item.className === "link";
+    });
+
+    links = newArray;
+
+    links.forEach(link => {
+      let element = document.querySelector(link.getAttribute("href"));
+      let linkST = ScrollTrigger.create({
+        trigger: element,
+        start: "-100px 0", // 위치
+      });
+
+      hamburgerArea.addEventListener("scroll", (e) => {
+        ScrollTrigger.create({
+          trigger: element,
+          start: "0% 50%",
+          end: "100% 50%",
+          onToggle: (self) => {
+            setActive(link);
+          },
+        });
+      });
+
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        gsap.to(hamburgerArea, {
+          duration: 1,
+          scrollTo: linkST.start,
+          overwrite: "auto"
+        });
+      });
+    });
+
+    function setActive(link) {
+      links.forEach((element) => {
+        element.classList.remove("active");
+      });
+
+      const targetPosition = sideNav.getBoundingClientRect();
+      if (52 === targetPosition.top) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      };
+    };
+  });
 });
--- assets/js/m_main.js
+++ assets/js/m_main.js
@@ -1,3 +1,35 @@
-window.addEventListener("load", function () {
+window.addEventListener("load", () => {
+  // --------------------------------
+  // 사용 목적：햄버거 메뉴 클릭 함수
+  // 사용 언어：javascript
+  // --------------------------------
+  const body = document.querySelector("body");
+  const hamburgerMenu = document.querySelector(".header .menu-button");
+  const hamburgerArea = document.querySelector(".hamburger-area");
+  const hamburgerClose = document.querySelector(".hamburger-area .side-button .close");
 
+  hamburgerMenu.addEventListener("click", (e) => {
+    e.preventDefault();
+    hamburgerArea.classList.add("active");
+    body.classList.add("overflow-y-hidden");
+
+    // 스크롤 시 클래스 추가
+    hamburgerArea.addEventListener("scroll", () => {
+      const sideNav = document.querySelector(".side-nav");
+
+      const targetPosition = sideNav.getBoundingClientRect();
+      if (targetPosition.top === 52) {
+        console.log("ok")
+        sideNav.classList.add("active");
+      } else {
+        sideNav.classList.remove("active");
+      }
+    });
+  });
+
+  hamburgerClose.addEventListener("click", (e) => {
+    e.preventDefault();
+    hamburgerArea.classList.remove("active");
+    body.classList.remove("overflow-y-hidden");
+  });
 });
