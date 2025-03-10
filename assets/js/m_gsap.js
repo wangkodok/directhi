c6d7f5d93b43aaf79a95f08ba91fa194cd7cc20c
wangkodok <wangkodok@gmail.com>
2025-03-10 20:25:20 +0900

    add：모바일 버전 콘텐츠 이동하는 네비게이션 영역

--- assets/js/m_gsap.js
+++ assets/js/m_gsap.js
@@ -1,5 +1,35 @@
 window.addEventListener("load", () => {
   // ------------------------------------------------------
+  // 사용 목적：네비게이션 드래그
+  // 사용 언어：gsap.js
+  // ------------------------------------------------------
+
+  // GSAP Draggable 플러그인 등록
+  gsap.registerPlugin(Draggable);
+
+  // 드래그 가능한 콘텐츠 선택
+  const dragContent = document.querySelector(".nav-link .list");
+  const container = document.querySelector(".nav-link");
+
+  // 컨테이너 및 콘텐츠 크기 계산
+  const containerStyle = getComputedStyle(container);
+  const paddingLeft = parseFloat(containerStyle.paddingLeft);
+  const paddingRight = parseFloat(containerStyle.paddingRight);
+  const marginLeft = parseFloat(containerStyle.marginLeft);
+  const marginRight = parseFloat(containerStyle.marginRight);
+
+  // Draggable 생성
+  Draggable.create(dragContent, {
+    type: "x",
+    bounds: {
+      minX: -(dragContent.offsetWidth - container.clientWidth + paddingRight + marginRight), // 오른쪽 끝 계산
+      maxX: paddingLeft + marginLeft, // 왼쪽 끝 계산
+    },
+    inertia: true, // 관성 효과
+    edgeResistance: 0.65, // 끝 저항력
+  })
+
+  // ------------------------------------------------------
   // 사용 목적：햄버거 메뉴 클릭 시 네비게이션 클릭 및 메뉴 이동
   // 사용 언어：javascript + gsap.js
   // ------------------------------------------------------
--- assets/js/m_main.js
+++ assets/js/m_main.js
@@ -1,5 +1,52 @@
 window.addEventListener("load", () => {
   // --------------------------------
+  // 사용 목적：네비게이션 이동
+  // 사용 언어：javascript
+  // --------------------------------
+  const header = document.querySelector(".header");
+  const targetElement = document.querySelector(".sec-insurance.car");
+  const navContentsLink = document.querySelector(".nav-link");
+  let lastScrollY = window.scrollY;
+
+
+  const scrollY = targetElement.offsetTop;
+
+  if (scrollY < lastScrollY) {
+    navContentsLink.classList.add("active");
+    header.classList.add("active");
+  } else {
+    navContentsLink.classList.remove("active");
+  }
+
+  window.addEventListener("scroll", () => {
+    const currentScrollY = window.scrollY;
+    const targetPosition = targetElement.offsetTop;
+
+    // 스크롤 위치가 특정 요소의 위치를 지나면 클래스 추가
+    if (currentScrollY >= 0 && currentScrollY <= targetPosition) {
+      navContentsLink.classList.remove("active")
+      if (currentScrollY < lastScrollY) {
+        // 위로 스크롤할 때
+        if (currentScrollY === 0) {
+          header.classList.remove("active");
+        } else {
+          header.classList.remove("active");
+        }
+      } else if (currentScrollY !== 0) {
+        // 아래로 스크롤할 때
+        header.classList.add("active");
+      }
+    } else {
+      // 구간을 벗어났을 때
+      header.classList.add("active");
+      navContentsLink.classList.add("active")
+    }
+
+    // 현재 스크롤 위치 저장
+    lastScrollY = currentScrollY;
+  });
+
+  // --------------------------------
   // 사용 목적：햄버거 메뉴 클릭 함수
   // 사용 언어：javascript
   // --------------------------------
